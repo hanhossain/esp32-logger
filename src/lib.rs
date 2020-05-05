040e0b2dd1a430fc43317b1560d2733a53fbbc4b
Han Hossain <ahanuf12@gmail.com>
2020-05-04 21:07:23 -0700

    convert it to a macro to allow formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 #![no_std]
 
-use core::fmt::Write;
 use esp32::{APB_CTRL, DPORT, RTCCNTL, UART0};
 use esp32_hal::clock_control::{ClockControl, XTAL_FREQUENCY_AUTO};
 use esp32_hal::dport::Split;
@@ -8,8 +7,8 @@ use esp32_hal::prelude::*;
 use esp32_hal::serial::config::Config;
 use esp32_hal::serial::{NoRx, NoTx, Rx, Serial, Tx};
 
-static mut STORED_TX: Option<Tx<UART0>> = None;
-static mut STORED_RX: Option<Rx<UART0>> = None;
+pub static mut STORED_TX: Option<Tx<UART0>> = None;
+pub static mut STORED_RX: Option<Rx<UART0>> = None;
 
 pub fn setup(uart0: UART0, rtccntl: RTCCNTL, apb_ctrl: APB_CTRL, dport: DPORT) {
     let (mut dport, dport_clock_control) = dport.split();
@@ -36,8 +35,12 @@ pub fn setup(uart0: UART0, rtccntl: RTCCNTL, apb_ctrl: APB_CTRL, dport: DPORT) {
     }
 }
 
-pub unsafe fn log(msg: &str) {
-    if let Some(tx) = &mut STORED_TX {
-        write!(tx, "{}\r\n", msg).unwrap();
-    }
+#[macro_export]
+macro_rules! log {
+    ($($arg:tt)*) => (
+        if let Some(tx) = unsafe { &mut STORED_TX } {
+            write!(tx, $($arg)*).unwrap();
+            write!(tx, "\r\n").unwrap();
+        }
+    );
 }
