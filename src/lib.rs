d91a347e6b588bf57cf711994ef254514ecdc29b
Han Hossain <ahanuf12@gmail.com>
2020-05-10 00:12:22 -0700

    make sure to return clock_control_config, watchdog, and dport to user

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![no_std]
 
+pub use core::fmt::Write;
 pub use esp32::{APB_CTRL, DPORT, RTCCNTL, UART0};
 pub use esp32_hal::clock_control::{ClockControl, XTAL_FREQUENCY_AUTO};
 pub use esp32_hal::dport::Split;
@@ -22,27 +23,31 @@ pub static STORED_RX: spin::Mutex<Option<Rx<UART0>>> = spin::Mutex::new(None);
 #[macro_export]
 macro_rules! setup_logger {
     ($dp:expr) => {
-        let (mut dport, dport_clock_control) = $dp.DPORT.split();
-        let clock_control =
-            ClockControl::new($dp.RTCCNTL, $dp.APB_CTRL, dport_clock_control, XTAL_FREQUENCY_AUTO).unwrap();
+        {
+            let (mut dport, dport_clock_control) = $dp.DPORT.split();
+            let clock_control =
+                ClockControl::new($dp.RTCCNTL, $dp.APB_CTRL, dport_clock_control, XTAL_FREQUENCY_AUTO).unwrap();
 
-        let (clock_control_config, mut watchdog) = clock_control.freeze().unwrap();
-        watchdog.disable();
+            let (clock_control_config, mut watchdog) = clock_control.freeze().unwrap();
+            watchdog.disable();
 
-        let serial = Serial::uart0(
-            $dp.UART0,
-            (NoTx, NoRx),
-            Config::default().baudrate(115200.Hz()),
-            clock_control_config,
-            &mut dport,
-        )
-        .unwrap();
+            let serial = Serial::uart0(
+                $dp.UART0,
+                (NoTx, NoRx),
+                Config::default().baudrate(115200.Hz()),
+                clock_control_config,
+                &mut dport,
+            )
+            .unwrap();
 
-        let (tx, rx) = serial.split();
+            let (tx, rx) = serial.split();
 
-        unsafe {
-            *STORED_TX.lock() = Some(tx);
-            *STORED_RX.lock() = Some(rx);
+            unsafe {
+                *STORED_TX.lock() = Some(tx);
+                *STORED_RX.lock() = Some(rx);
+            }
+
+            (clock_control_config, watchdog, dport)
         }
     }
 }
